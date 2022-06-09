7f111762777e6698412275bee69348671ca037ea
sjperez1 <sarahpz479@gmail.com>
2022-06-08 22:55:22 -0600

    adjusted code

--- server.py
+++ server.py
@@ -6,18 +6,22 @@ def say_hello_world():
     return "Hello World!"
 
 # For the functions, how do you know when to use print vs return in these?
-# When is the following if line needed?
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+
+# is adding string:  what it meant when saying to make sure the names are strings in the bonus?
+@app.route('/say/<string:title>')
+def hi_name(title):
+    return "Hi " + title + "!"
+
+# is this what it meant in the bonus by saying make sure one is an integer and one is a string?
+@app.route('/<int:number>/<string:word>')
+def multiply(number, word):
+    return word * number
+
+# When is the following if line needed? Is this how you add that very last else statement?
 if __name__=="__main__":
-    @app.route('/dojo')
-    def dojo():
-        return "Dojo!"
-
-    @app.route('/say/<title>')
-    def hi_name(title):
-        return "Hi " + title + "!"
-
-    @app.route('/<int:number>/<string:word>')
-    def multiply(number, word):
-        return word * number
-
     app.run(debug=True)
+else:
+    print("Sorry! No response. Try again.")
