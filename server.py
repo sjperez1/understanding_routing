8c797841e87a80087055d6fe376f4257e2238426
sjperez1 <sarahpz479@gmail.com>
2022-06-09 13:18:14 -0600

    adjusted comments

--- server.py
+++ server.py
@@ -5,23 +5,21 @@ app = Flask(__name__)
 def say_hello_world():
     return "Hello World!"
 
-# For the functions, how do you know when to use print vs return in these?
+# Use print when things should show up just in terminal and use return when things should display on the webpage.
 @app.route('/dojo')
 def dojo():
     return "Dojo!"
 
-# is adding string:  what it meant when saying to make sure the names are strings in the bonus?
 @app.route('/say/<string:title>')
 def hi_name(title):
     return "Hi " + title + "!"
 
-# is this what it meant in the bonus by saying make sure one is an integer and one is a string?
 @app.route('/<int:number>/<string:word>')
 def multiply(number, word):
     return word * number
 
-# When is the following if line needed? Is this how you add that very last else statement?
 if __name__=="__main__":
     app.run(debug=True)
+# The following is not correct
 else:
     print("Sorry! No response. Try again.")
